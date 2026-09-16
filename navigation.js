d20463eeed791de0ee34eec541752c4e3173ccaa
Alexandre Battut <alexandre.battut.pro@gmail.com>
2026-09-16 15:25:04 +0200

    tries to fix nav

--- navigation.js
+++ navigation.js
@@ -1,5 +1,39 @@
 const sections = document.querySelectorAll(".page-section");
 const navDots = document.querySelectorAll(".nav-dot");
+
+function updateActiveSection(){
+    const screenCenter = window.innerHeight/2;
+    const closestSection = null;
+    const closestDistance = Infinity;
+
+    sections.forEach(function(section){
+        const rect = section.getBoundingClientRect();
+        const sectionCenter = rect.top + (rect.height/2);
+        const distance = Math.abs(screenCenter - sectionCenter);
+        if (distance < closestDistance) {
+            closestDistance = distance;
+            closestSection = section;
+        }
+    })
+
+    if (!closestSection) {
+        return;
+    }
+
+    navDots.forEach(function (dot) {
+        dot.classList.remove("active");
+    });
+
+    const activeDot = document.querySelector(`.nav-dot[href="#${closestSection.id}"]`);
+
+    if (activeDot) {
+        activeDot.classList.add("active");
+    }
+    else console.log("false");
+
+}
+
+
 const observer = new IntersectionObserver(
     function (entries) {
         entries.forEach(function (entry) {
